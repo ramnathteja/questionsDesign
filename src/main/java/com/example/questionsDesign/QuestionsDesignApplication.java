02e7155633a2f143a25b079f37937388808b9baa
ramnathteja <ch.ramnathteja@gmail.com>
2025-08-17 22:01:47 +0530

    initial git commit

---
+++ src/main/java/com/example/questionsDesign/QuestionsDesignApplication.java
@@ -0,0 +1,13 @@
+package com.example.questionsDesign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuestionsDesignApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuestionsDesignApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/questionsDesign/QuestionsDesignApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.questionsDesign;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuestionsDesignApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
